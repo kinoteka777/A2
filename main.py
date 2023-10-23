112ff85379801f6d8b4c2c8e6a90f9fe269c35cb
Kinoteka777 <caseyfaggion1@gmail.com>
2023-10-23 21:47:07 +1030

    headers, basic class layout implemented

---
+++ main.py
@@ -0,0 +1,25 @@
+# File: main.py
+# Description: the main file for UniSA OOP Assessment 2, Study period 5.
+# Author: Casey Faggion
+# StudentID: 110398039
+# EmailID: fagck001
+# This is my own work as defined by the University's Academic Misconduct Policy.
+
+
+class Alchemist:
+    pass
+
+class Laboratory:
+    pass
+
+class Potion:
+    pass
+
+class Reagent:
+    pass
+
+class SuperPotion(Potion):
+    pass
+
+class ExtremePotion(Potion):
+    pass
---
+++ test.py
@@ -0,0 +1,6 @@
+# File: main.py
+# Description: the auxiliary file for the testing of the functions of main.py for UniSA OOP A2, Study Period 5.
+# Author: Casey Faggion
+# StudentID: 110398039
+# EmailID: fagck001
+# This is my own work as defined by the University's Academic Misconduct Policy.
